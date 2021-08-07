31cfbf7bddbba53c5dfa20c58e8c94ba9a56249a
Olga Zhurova <82470486+ozhurova@users.noreply.github.com>
2021-08-07 20:37:50 +0300

    created Details game, add lib Angularjs-gauge for rating, refactoring types, changed API-key, API-host

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,38 +1,54 @@
-import { Component,  OnInit } from '@angular/core';
-import {APIResponse, Game} from "../../models";
-import {HttpService} from "../../services/http.service";
-import {ActivatedRoute, Params} from "@angular/router";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { APIResponse, Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  public games: Array<Game> | undefined;
-  public sort: string | undefined;
-
+export class HomeComponent implements OnInit, OnDestroy {
+  public sort: string;
+  public games: Game[];
+  private routeSub: Subscription;
+  private gameSub: Subscription;
 
   constructor(
     private httpService: HttpService,
-    private activatedRoute: ActivatedRoute
-  ) {}
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ){}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params: Params) => {
-      if(params['game-search']) {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params) => {
+      if (params['game-search']) {
         this.searchGames('metacrit', params['game-search']);
       } else {
         this.searchGames('metacrit');
       }
     });
   }
+
   searchGames(sort: string, search?: string): void {
-    this.httpService
+    this.gameSub = this.httpService
       .getGameList(sort, search)
       .subscribe((gameList: APIResponse<Game>) => {
         this.games = gameList.results;
-        console.log(this.games.map(i=>i.parent_platforms))
-      })
+      });
+  }
+
+  openGameDetails(id: number): void {
+    this.router.navigate(['details', id]);
+  }
+
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
   }
 }
